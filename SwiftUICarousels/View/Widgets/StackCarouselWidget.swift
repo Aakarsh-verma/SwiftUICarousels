5e7e3e8ec1fc930108786865ec0651747194bb0d
Aakarsh Verma <aakarshkun@gmail.com>
2025-06-01 14:17:42 +0530

    Generalisation of Stack Carousel Widget

--- SwiftUICarousels/View/Widgets/StackCarouselWidget.swift
+++ SwiftUICarousels/View/Widgets/StackCarouselWidget.swift
@@ -7,21 +7,33 @@
 
 import SwiftUI
 
-struct StackCarouselWidget: View {
+struct StackCarouselWidget<T: Identifiable, Content: View>: View {
     @State private var currentIndex = 0
-    @EnvironmentObject private var viewModel: HomeViewModel
+    
+    var items: [T]
+    var height: CGFloat = CGFloat(240)
+    var content: (T) -> Content
+    var perform: (() async -> Void)? = nil
     
     var body: some View {
-        StackCarouselView(items: viewModel.animeImages.isEmpty ? sampleImages : viewModel.animeImages, currentIndex: $currentIndex) { imageModel in
-            CustomImageView(imageModel: CustomImageModel(for: imageModel.image))
-                .scaledToFit()
-                .clipShape(.rect(cornerRadius: 20))
+        StackCarouselView(items: items, currentIndex: $currentIndex) { item in
+            content(item)
+        } action: { _ in
         }
-        .frame(height: 240)
+        .frame(height: height)
+        .task {
+            if items.isEmpty {
+                await perform?()
+            }
+        }
     }
 }
 
 #Preview {
-    StackCarouselWidget()
-        .environmentObject(HomeViewModel())
+    StackCarouselWidget(items: sampleImages) { imageModel in
+        CustomImageView(imageModel: CustomImageModel(for: imageModel.image))
+            .scaledToFit()
+            .clipShape(.rect(cornerRadius: 20))
+    }
 }
+
