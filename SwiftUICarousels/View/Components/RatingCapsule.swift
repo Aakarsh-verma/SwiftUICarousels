122320c094a130eb05f047febcd36cb8efb9cc4a
Aakarsh Verma <aakarshkun@gmail.com>
2025-07-15 00:01:33 +0530

    rating UI genericfication

--- SwiftUICarousels/View/Components/RatingCapsule.swift
+++ SwiftUICarousels/View/Components/RatingCapsule.swift
@@ -11,20 +11,25 @@ struct RatingCapsule: View {
     var rating: String = "5"
     var color: Color = .white
     var borderColor: Color = .white
+    var showBorder: Bool = true
     
     var body: some View {
         HStack(spacing: 12) {
-            Label(rating, systemImage: "star")
+            Label(rating, systemImage: "star.fill")
                 .font(.system(size: UIFont.preferredFont(forTextStyle: .subheadline).pointSize))
                 .foregroundStyle(color)
                 .padding(.vertical, 4)
                 .padding(.horizontal, 8)
         }
-        .overlay {
-            Capsule(style: .continuous)
-                .fill(.clear)
-                .stroke(borderColor.secondary, style: .init(lineWidth: 2))
-        }    }
+        .applyIf(showBorder, modifier: { view in
+            view
+                .overlay {
+                    Capsule(style: .continuous)
+                        .fill(.clear)
+                        .stroke(borderColor.secondary, style: .init(lineWidth: 2))
+                } 
+        })
+    }
 }
 
 #Preview {
