e54c13beebab8d024695cb26a48706724c1166dc
Aakarsh Verma <aakarshkun@gmail.com>
2025-07-15 00:01:57 +0530

    Custom Grid View with Grid Card

--- SwiftUICarousels/View/GridView/CustomGridView.swift
+++ SwiftUICarousels/View/GridView/CustomGridView.swift
@@ -7,12 +7,72 @@
 
 import SwiftUI
 
-struct CustomGridView: View {
+struct CustomGridView<Data: RandomAccessCollection, Content: View>: View where Data.Element: Identifiable {
+    let items: Data
+    let itemSizeRatio: Double
+    let layout: [GridItem]
+    let isVertical: Bool 
+    var content: (Data.Element) -> Content
+    var action: (Data.Element) -> Void
+    
+    init(items: Data,
+         numberOfLayout: Int = 2,
+         itemSizeRatio: Double = 0.475,
+         isVertical: Bool = true,
+         content: @escaping (Data.Element) -> Content, 
+         action: @escaping (Data.Element) -> Void) {
+        self.items = items
+        self.itemSizeRatio = itemSizeRatio
+        self.layout = Array(repeating: GridItem(.flexible()), count: numberOfLayout)
+        self.isVertical = isVertical
+        self.content = content
+        self.action = action
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            let cardWidth: CGFloat = CGFloat(proxy.size.width * itemSizeRatio)
+            
+            if isVertical {
+                LazyVGrid(columns: layout, spacing: 12) { 
+                    GridLayout(cardWidth: cardWidth)                
+                }
+            } else {
+                ScrollView(.horizontal ,showsIndicators: false) {
+                    LazyHGrid(rows: layout, spacing: 12) { 
+                        GridLayout(cardWidth: cardWidth)
+                    }
+                }
+            }
+            
+        }
+    }
+    
+    @ViewBuilder 
+    func GridLayout(cardWidth: CGFloat) -> some View {
+        ForEach(Array(items.enumerated()), id: \.1.id) { index, item in
+            ItemView(item, for: index, with: cardWidth)
+        }
+    }
+    
+    @ViewBuilder
+    private func ItemView(_ item: Data.Element, for index: Int, with cardWidth: CGFloat) -> some View {
+        content(item)
+            .frame(width: cardWidth)            
+            .onTapGesture {
+                action(item)
+            }
     }
 }
 
 #Preview {
-    CustomGridView()
+    ScrollView {
+        CustomGridView(items: sampleImages) { imageModel in
+            CustomImageView(imageModel: CustomImageModel(for: imageModel.image))
+                .scaledToFit()
+                .clipShape(.rect(cornerRadius: 20))
+            
+        } action: {_ in}
+            .padding(.horizontal)
+    }
 }
---
+++ SwiftUICarousels/View/GridView/GridCard.swift
@@ -0,0 +1,104 @@
+//
+//  GridCard.swift
+//  SwiftUICarousels
+//
+//  Created by Aakarsh Verma on 12/07/25.
+//
+
+import SwiftUI
+
+struct GridCard: View {
+    @State private var wishlisted: Bool = false
+    @Binding var path: NavigationPath
+    @Binding var content: CardModel
+    
+    private let fontScale: CGFloat = 1.2
+    
+    var body: some View {
+        ZStack {
+            CustomImageView(imageModel: content.image)
+                .aspectRatio(contentMode: .fit)
+                .clipShape(.rect(cornerRadius: 20))
+                .overlay {
+                    gradientView
+                    VStack {
+                        HStack {
+                            Spacer()
+                            wishlistIcon()
+                        }
+                        Spacer()
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Spacer()
+                        VStack(alignment: .leading) {
+                            textView()
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 20)
+                        .padding(.bottom)
+                    }
+                }
+        }
+        .onTapGesture {
+            path.append(content)
+        }
+    }
+    
+    var gradientView: some View {
+        VStack {
+            Spacer()
+            
+            Rectangle()
+                .fill(LinearGradient(colors: [
+                    .clear,
+                    .black.opacity(0.3),
+                    .black.opacity(0.4),
+                    .black.opacity(0.4),
+                    .black.opacity(0.5),
+                    .black.opacity(0.6),
+                    .black.opacity(0.8),
+                    .black
+                ], startPoint: .top, endPoint: .bottom))
+        }
+        .clipShape(.rect(cornerRadius: 20))
+    }
+    
+    @ViewBuilder
+    private func wishlistIcon() -> some View {
+        let iconModel = IconModel(
+            name: "heart",
+            type: .tertiary,
+            size: .Regular,
+            color: .white,
+            bgColor: .clear,
+            tapAction: {
+                content.isWishlisted.toggle()
+                wishlisted = content.isWishlisted
+            })
+        IconView(with: iconModel, isFilled: $wishlisted)
+            .padding(.top)
+            .padding(.trailing)
+    }
+    
+    @ViewBuilder
+    private func textView() -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(content.title)
+                .font(.system(size: UIFont.preferredFont(forTextStyle: .title2).pointSize, weight: .bold))
+                .lineLimit(2)
+                .truncationMode(.tail)
+                .multilineTextAlignment(.leading)
+                .foregroundColor(.white)
+            
+            RatingCapsule(rating: content.rating, showBorder: false)
+                .padding(.leading, -8)
+        }
+        
+    }
+}
+
+#Preview {
+    GridCard(path: .constant(.init()), content: .constant(.init()))
+        .frame(width: UIScreen.main.bounds.width / 2)
+}
