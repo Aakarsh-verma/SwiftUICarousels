89aa3a4b71367d41e09bd67603dae0f47c2be045
Aakarsh Verma <aakarshkun@gmail.com>
2026-07-28 00:46:30 +0530

    glass UI improvements

--- SwiftUICarousels/Utility/UI/Extenstions.swift
+++ SwiftUICarousels/Utility/UI/Extenstions.swift
@@ -73,4 +73,28 @@ extension View {
             self
         }
     }
+    
+    @ViewBuilder
+    func glassBackground<S: Shape>(
+        _ shape: S
+    ) -> some View {
+        modifier(
+            GlassEffectModifier(shape: shape)
+        )
+    }
+}
+
+private struct GlassEffectModifier<S: Shape>: ViewModifier {
+    let shape: S
+
+    @ViewBuilder
+    func body(content: Content) -> some View {
+        if #available(iOS 26.0, *) {
+            content
+                .glassEffect(.regular, in: shape)
+        } else {
+            content
+                .background(.ultraThinMaterial, in: shape)
+        }
+    }
 }
